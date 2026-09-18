41dc298217418418949580a2d5e906c9a2d7d2cd
osama-bq <osamaqashem@gmail.com>
2026-09-18 19:16:17 +0600

    Implement Player component for managing player names and symbols

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,14 @@
+import Player from "./components/Player"
 
 function App() {
-  
-
-  return (
-    <p>Coming soon...</p>
-  )
+  return <main>
+    <div id="game-container">
+      <lo id="players">
+        <Player initialName="Player 1" symbol="X" />
+        <Player initialName="Player 2" symbol="O" />
+      </lo>
+    </div>
+  </main>
 }
 
 export default App
