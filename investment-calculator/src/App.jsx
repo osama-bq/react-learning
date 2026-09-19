3e9bafa7e107253a6f77811bc21bcfce7df3d2ab
osama-bq <osamaqashem@gmail.com>
2026-09-19 13:43:45 +0600

    Add input validation for duration in App component

--- investment-calculator/src/App.jsx
+++ investment-calculator/src/App.jsx
@@ -12,6 +12,8 @@ function App() {
     duration: 10
   });
 
+  const inputIsValid = userInput.duration > 0;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput(prevUserInput => {
       return {
@@ -25,7 +27,8 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && <p className="center">Please enter a valid duration (greater than 0).</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   )
 }
