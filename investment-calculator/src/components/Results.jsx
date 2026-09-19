fd6b33854ea5c8cc25fc570ffd99ab4532ff11f3
osama-bq <osamaqashem@gmail.com>
2026-09-19 13:41:30 +0600

    Add Results component and integrate user input handling in App component

---
+++ investment-calculator/src/components/Results.jsx
@@ -0,0 +1,34 @@
+import { calculateInvestmentResults, formatter } from '../util/investment.js';
+
+export default function Results({ input }) {
+    const results = calculateInvestmentResults(input);
+    const initialInvestment = results[0].valueEndOfYear - results[0].interest - results[0].annualInvestment;
+
+    return <table id="result">
+        <thead>
+            <tr>
+                <th>Year</th>
+                <th>Investment Value</th>
+                <th>Interest (Year)</th>
+                <th>Total Interest</th>
+                <th>Invested Capital</th>
+            </tr>
+        </thead>
+        <tbody>
+            {results.map(yearData => {
+                const totalInterest = yearData.valueEndOfYear - initialInvestment - (yearData.year * yearData.annualInvestment);
+                const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+
+                return (
+                    <tr key={yearData.year}>
+                        <td>{yearData.year}</td>
+                        <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                        <td>{formatter.format(yearData.interest)}</td>
+                        <td>{formatter.format(totalInterest)}</td>
+                        <td>{formatter.format(totalAmountInvested)}</td>
+                    </tr>
+                );
+            })}
+        </tbody>
+    </table>;
+}
