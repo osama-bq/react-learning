644c37114d410e159ef9d93d8c36afc03514ef26
osama-bq <osamaqashem@gmail.com>
2026-09-21 14:28:25 +0600

    React Quiz Project Finished

---
+++ react-quiz/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/Header.jsx';
+import Quiz from './components/Quiz.jsx';
+
+function App() {
+    return <>
+        <Header />
+        <Quiz />
+    </>;
+}
+
+export default App;
