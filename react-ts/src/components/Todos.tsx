5f914302193ae78cdf1d061419a6ccb1d4ab5133
Osama <osamaqashem@gmail.com>
2026-09-24 19:58:21 +0600

    Completed

---
+++ react-ts/src/components/Todos.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Todo from '../models/todo';
+import TodoItem from './TodoItem';
+
+import { useContext } from 'react';
+import { TodosContext } from '../store/todos-context';
+
+import classes from './Todos.module.css';
+
+const Todos: React.FC = () => {
+    const todosCtx = useContext(TodosContext);
+
+    return (<ul className={classes.todos}>
+        {todosCtx.items.map((item) => (
+            <TodoItem key={item.id} text={item.text} onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)} />
+        ))}
+    </ul>);
+}
+
+export default Todos;
